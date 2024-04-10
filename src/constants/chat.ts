76ca871c967010ffce95d219fa4125080a46ab38
lerejbox <jerelongbx@gmail.com>
2024-04-11 02:35:11 +0800

    updated new models

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -24,12 +24,11 @@ export const modelOptions: ModelOptions[] = [
   'gpt-3.5-turbo-0125',
   'gpt-4',
   'gpt-4-32k',
+  'gpt-4-0613',
   'gpt-4-1106-preview',
   'gpt-4-0125-preview',
-  'gpt-4-vision-preview'
-  // 'gpt-3.5-turbo-0301',
-  // 'gpt-4-0314',
-  // 'gpt-4-32k-0314',
+  'gpt-4-vision-preview',
+  'gpt-4-turbo-2024-04-09'
 ];
 
 export const defaultModel = 'gpt-3.5-turbo';
